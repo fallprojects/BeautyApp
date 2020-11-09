ff3c454ce6fe92cfcade35c0e910515fcb21cabd
SuperAlina2020 <alinagithub@gmail.com>
2020-11-09 19:33:10 +0600

    import all new models from models

--- beauty/room/views.py
+++ beauty/room/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def home_page(request):
+    service = Service.objects.all()
+    feedback = Feedback.objects.all()
+    contact = Contacts.objects.all()
+    master = Masters.objects.all()
+    certificate = Certificate.objects.all()
+    context = {'service':service,'feedback':feedback,'contact':contact,'master':master,'certificate':certificate}
+    return render(request,'room/home.html',context)
+
+
+def service_page(request):
+    service = Service.objects.all()
+    context = {'service':service}
+    return render(request,'room/service.html',context)
+
+
+def feedback_page(request):
+    feedback = Feedback.objects.all()
+    context = {'feedback':feedback}
+    return render(request,'room/feedback.html',context)
+
+
+def contact_page(request):
+    contact = Contacts.objects.all()
+    context = {'contact':contact}
+    return render(request,'room/contact.html',context)
+
+
+
