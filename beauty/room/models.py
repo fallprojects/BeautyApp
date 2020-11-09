bb674ec4842b520150798340789ea720427d4446
SuperAlina2020 <alinagithub@gmail.com>
2020-11-09 19:32:33 +0600

    added new models Service,Feedback,Contacts,Masters,Certificate

--- beauty/room/models.py
+++ beauty/room/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    description = models.CharField(max_length=100,null=True)
+    price = models.FloatField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    full_name = models.CharField(max_length=100,null=True)
+    description = models.CharField(max_length=100,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+
+    def __str__(self):
+        return self.full_name
+
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    full_name = models.CharField(max_length=100,null=True)
+    phone = models.CharField(max_length=100,null=True)
+    email = models.CharField(max_length=100,null=True)
+
+
+    def __str__(self):
+        return self.full_name
+
+
+class Masters(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    description = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    description = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
