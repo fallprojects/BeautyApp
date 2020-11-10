4fc113a9510aee3787ddcab05547efec70caf4f4
SuperAlina2020 <alinagithub@gmail.com>
2020-11-10 19:37:11 +0600

    added new function service detail

--- beauty/room/views.py
+++ beauty/room/views.py
@@ -4,29 +4,47 @@ from .models import *
 def home_page(request):
     service = Service.objects.all()
     feedback = Feedback.objects.all()
-    contact = Contacts.objects.all()
-    master = Masters.objects.all()
+    contact = Contact.objects.all()
+    master = Master.objects.all()
     certificate = Certificate.objects.all()
     context = {'service':service,'feedback':feedback,'contact':contact,'master':master,'certificate':certificate}
     return render(request,'room/home.html',context)
 
 
 def service_page(request):
-    service = Service.objects.all()
-    context = {'service':service}
+    services = Service.objects.all()
+    context = {'services':services}
     return render(request,'room/service.html',context)
 
 
 def feedback_page(request):
-    feedback = Feedback.objects.all()
-    context = {'feedback':feedback}
+    feedbacks = Feedback.objects.all()
+    context = {'feedbacks':feedbacks}
     return render(request,'room/feedback.html',context)
 
 
 def contact_page(request):
-    contact = Contacts.objects.all()
-    context = {'contact':contact}
+    contacts = Contact.objects.all()
+    context = {'contacts':contacts}
     return render(request,'room/contact.html',context)
+
+def master_page(request):
+    masters = Master.objects.all()
+    context = {'masters':masters}
+    return render(request,'room/master.html',context)
+
+
+def certificate_page(request):
+    certificates = Certificate.objects.all()
+    context = {'certificates':certificates}
+    return render(request,'room/certificate.html',context)
+
+
+def service_detail(request,pk):
+    detail = Service.objects.get(id=pk)
+    context = {'detail':detail}
+    return render(request,'room/details.html',context)
+
 
 
 
